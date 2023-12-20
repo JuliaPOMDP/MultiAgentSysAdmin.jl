4f4ee05b9bc66ebbfd87c11ccad52289d61a325b
Dylan Asmar <asmar@stanford.edu>
2023-12-20 00:44:59 -0700

    repo url change

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,12 @@ using MultiAgentSysAdmin
 using Documenter
 
 makedocs(;
-    modules=[MultiAgentSysAdmin],
-    authors="rejuvyesh <mail@rejuvyesh.com> and contributors",
-    repo="https://github.com/rejuvyesh/MultiAgentSysAdmin.jl/blob/{commit}{path}#L{line}",
     sitename="MultiAgentSysAdmin.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://rejuvyesh.github.io/MultiAgentSysAdmin.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    authors="rejuvyesh <mail@rejuvyesh.com> and contributors",
+    modules=[MultiAgentSysAdmin],
+    format=Documenter.HTML()
 )
 
 deploydocs(;
-    repo="github.com/rejuvyesh/MultiAgentSysAdmin.jl",
+    repo="github.com/JuliaPOMDP/MultiAgentSysAdmin.jl.git",
 )
